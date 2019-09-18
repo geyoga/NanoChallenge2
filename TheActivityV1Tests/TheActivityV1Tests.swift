f5791fc972af291523bcf0287ca11f76d9dd4f07
Georgius Yoga Dewantama <yogadewantama@gmail.com>
2019-09-18 10:50:35 +0700

    Initial Commit

---
+++ TheActivityV1Tests/TheActivityV1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TheActivityV1Tests.swift
+//  TheActivityV1Tests
+//
+//  Created by Georgius Yoga Dewantama on 18/09/19.
+//  Copyright © 2019 Georgius Yoga Dewantama. All rights reserved.
+//
+
+import XCTest
+@testable import TheActivityV1
+
+class TheActivityV1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
